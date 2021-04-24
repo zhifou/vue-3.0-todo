6487ad92d54675f139c552d51f99374ab4aeb302
zhaoyadong <zhaoyadong@baidu.com>
2021-04-24 21:26:50 +0800

    升级了配置vuex和vite

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,30 @@
-import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router';
-import Index from '../views/index.vue';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Index from "../views/index.vue";
 // import todoList from '../views/todo-list.vue';
-import swr from '../views/swr.vue';
+import swr from "../views/swr.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {
-        path: '/',
-        name: 'home',
-        component: Index
-    }, {
-        path: '/todo',
-        name: 'todo',
-        component: () => import('../views/todo-list.vue')
-    }, {
-        path: '/swr',
-        name: 'swr',
-        component: swr
-    }
+        path: "/",
+        name: "home",
+        component: Index,
+    },
+    {
+        path: "/todo",
+        name: "todo",
+        component: () => import("../views/todo-list.vue"),
+    },
+    {
+        path: "/swr",
+        name: "swr",
+        component: swr,
+    },
 ];
 
 // 在 Vue-router新版本中，需要使用createRouter来创建路由
 export default createRouter({
-  // 指定路由的模式,此处使用的是hash模式
-  history: createWebHashHistory(),
-  // 路由地址
-  routes
+    // 指定路由的模式,此处使用的是hash模式
+    history: createWebHashHistory(),
+    // 路由地址
+    routes,
 });
